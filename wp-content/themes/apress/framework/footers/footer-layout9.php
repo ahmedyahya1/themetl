ba91ae3e2a84493091033c0c20a2f158106db69a
Ahmed Yahya <ahmed.yahya@ibm.com>
2019-12-16 17:34:25 +0200

    yalla

--- wp-content/themes/apress/framework/footers/footer-layout9.php
+++
@@ -1,7 +0,0 @@
- <?php global $apress_data, $woocommerce; ?>
-
-<?php if ( is_active_sidebar( 'footer_widget1' ) ) { ?>
-<div class="zolo_footer_row">
-<div class="footercolumn footer_column1"><?php dynamic_sidebar('footer_widget1'); ?></div>
-</div>
-<?php }?>
